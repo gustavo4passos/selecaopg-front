09d09ca178a252d5ddec59bf223e5ab4a97e3fdb
Victor Pinheiro <vpinheiro.aguiar@gmail.com>
2019-11-12 09:47:07 -0300

    Add SnackBar error message
    - Add SnackBar error message on SignIn and SignUp
    - Fix SignIn form validation
    - Update constants list

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -7,4 +7,9 @@ export const constants = {
     btnSignup: 'Cadastrar',
     signupTitle: 'Registrar',
     loginTitle: 'Entrar',
+    errorServer: 'Houve um erro na conexão com o servidor. Tente novamente mais tarde!',
+    fieldRequired: 'Esse campo é obrigatório',
+    emailIncorrect: 'Isso não parece um email',
+    passwordShort: 'Mínimo de 6 caracteres',
+    passwordDifferent: 'As senhas não são iguais'
 }
