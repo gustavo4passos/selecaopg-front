7c70119e954446be19c827b54c9cb3ea7041aacd
Igor de Almeida <igoor1205@gmail.com>
2019-11-14 11:35:02 -0300

    Add link to backend

--- src/services/defaultProps.js
+++ src/services/defaultProps.js
@@ -2,4 +2,4 @@ console.log('NODE_ENV', process.env.NODE_ENV)
 
 export const API = process.env.NODE_ENV !== 'production' ? 
     'http://127.0.0.1:3333/': 
-    'http://127.0.0.1:3333/'
+    'http://selecaopgcomp-back.herokuapp.com/'
